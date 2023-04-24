60ae6495b1ab1cfe25f8b76d994600a592714c57
YebraL <Product4conker@gmail.com>
2023-04-24 10:58:25 -0500

    basic python

---
+++ basic.py
@@ -0,0 +1,50 @@
+# print('Hello world')
+# user_text = input('give me user text: ')
+# print(type(user_text))
+# number = 10
+# print(type(number))
+# lst1 = ['one', 'two', 'three']
+# print(lst1)
+
+# index = 1
+# while(10 > index):
+#     print(index)
+#     index += 1
+
+# todo_list = []
+# check = True
+# while check:
+#     todo = input('Add to the Todo list: ')
+#     if todo == 'stop': check = False
+#     todo_list.append(todo)
+#     print(todo_list)
+    
+# -----------Code Exercise 1--------------- 
+# Create a program that prompts the user to input their name once. 
+# Then, the program prints out the name once with the first letter
+# capitalized.
+
+# prompt = input('give your input here: ')
+# print(prompt.title())
+
+# -----------Code Exercise 2--------------- 
+
+# Create a program that prompts the user to input their name once. 
+# Then, the program repeatedly prints out the name with the first 
+# letter capitalized.
+
+# name = input('Your name to troll: ')
+# while True:
+#     print(name.title())
+
+# -----------Code Exercise 3--------------- 
+
+# Create a program that prompts the user to input their name repeatedly. 
+# Then, the program repeatedly prints out the name with the first letter 
+# capitalized.
+
+# while True:
+#     name = input('Whats your name? ')
+#     print(name.title())
+
+# switch
